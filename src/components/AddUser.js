b6e8153fc29550aa56446dbd4c2c73b7724aaf05
Skanda <sskanda98@gmail.com>
2023-06-09 19:36:54 +0530

    more changes

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AddUser.css";
 
 function AddUser() {
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
   const submithandler = (e) => {
     e.preventDefault();
-    console.log("dafaq");
+    console.log(enteredName, enteredAge);
+    setEnteredName("");
+    setEnteredAge("");
+  };
+  const namehandler = (e) => {
+    setEnteredName(e.target.value);
+  };
+  const agehandler = (e) => {
+    setEnteredAge(e.target.value);
   };
 
   return (
     <div className="add-user">
       <form id="form-container" onSubmit={submithandler}>
         <label>Username</label>
-        <input type="text"></input>
+        <input
+          type="text"
+          id="name"
+          onChange={namehandler}
+          value={enteredName}
+        ></input>
         <label>Age</label>
-        <input type="text"></input>
+        <input
+          type="text"
+          id="age"
+          onChange={agehandler}
+          value={enteredAge}
+        ></input>
         <button>Add User</button>
       </form>
     </div>
